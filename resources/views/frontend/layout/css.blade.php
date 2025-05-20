c2471b8fa8b11f20db729dfdcc94f379946a8a1a
yuzgec <olcayy@gmail.com>
2025-05-20 08:18:30 +0300

    Fix

--- resources/views/frontend/layout/css.blade.php
+++ resources/views/frontend/layout/css.blade.php
@@ -1,4 +1,9 @@
 <link rel="shortcut icon" href="/{{ config('settings.siteFavIcon')}}" type="image/x-icon" />
 <link rel="apple-touch-icon" href="/{{ config('settings.siteFavIcon')}}">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
-<link rel="stylesheet" href="/frontend/css/style.css?ver=5">
+<link rel="stylesheet" href="/frontend/css/style.css?ver=7">
+
+<link rel="alternate" hreflang="tr" href="https://urbankabin.com.tr/" />
+<link rel="alternate" hreflang="en" href="https://urbankabin.com.tr/en/" />
+<link rel="alternate" hreflang="ar" href="https://urbankabin.com.tr/sa/" />
+<link rel="alternate" hreflang="x-default" href="https://urbankabin.com.tr/" />
